2373fd48300aeca793f2c5e152280a8658ae0816
Ricardo Delfin Garcia <ricardo.delfin.garcia@gmail.com>
2018-01-07 21:52:46 -0600

    Added documentation to obj_loader.hpp

--- include/maguey/obj_loader.hpp
+++ include/maguey/obj_loader.hpp
@@ -15,9 +15,35 @@
 
 namespace maguey {
 
+/**
+ * Class used to load in a triangle mesh from a Wavefront .obj file.
+ * 
+ * The current version is very barebones and only loads in vertices, normals,
+ * and faces. It will ignore all material, section, or any other data. It does
+ * however, process quads and turns them to triangles.
+ */
 class ObjLoader : public MeshLoader {
 public:
     ObjLoader();
+    
+    /**
+     * Uses the contents string to fill up the three vectors with the vertex
+     * data stored in the Wavefront .obj file. Returns whether the operation
+     * succeded or not. Notably, it'll return false if it finds an invalid
+     * line. In this case, the contents of vertices, normals, and faces are not
+     * guaranteed to be in their initial state.
+     * 
+     * @param contents The contents of the file format being loaded in.
+     * @param vertices A reference to the vertice vector that will be edited to
+     *                 contain the vertices specified in the contents.
+     * @param normals  A reference to the normal vector that will be edited to
+     *                 contain the normals specified in the contents.
+     * @param faces    A reference to the face vector that will be edited to
+     *                 contain the indices of the vertices and normals that
+     *                 make up the faces specified in the contents.
+     *                 Specifically, each index in each face corresponds to
+     *                 both the vertex and normal.
+     */
     virtual bool loadString(const std::string& contents,
                             std::vector<glm::vec4>& vertices, std::vector<glm::vec4>& normals,
                             std::vector<glm::uvec3>& faces) const;
