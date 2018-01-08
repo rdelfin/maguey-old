4f2bfbf0f9e245b0d2512ffb45cefe7066ba4a08
Ricardo Delfin Garcia <ricardo.delfin.garcia@gmail.com>
2018-01-07 22:12:07 -0600

    Added documentation to renderable.hpp

--- include/maguey/renderable.hpp
+++ include/maguey/renderable.hpp
@@ -8,12 +8,19 @@
 
 namespace maguey {
 
+/**
+ * Interface representing a basic renderable object, such as a triangle mesh or
+ * a geometric shape.
+ */
 class Renderable {
 public:
     Renderable()  {
 
     }
 
+    /**
+     * Pure virtual function to be called to render the object on the screen.
+     */
     virtual void draw() = 0;
 
     virtual ~Renderable() {
