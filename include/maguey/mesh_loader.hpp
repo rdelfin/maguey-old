a6cf60e686a088223cfc01c016fd7dca59d1fd74
Ricardo Delfin Garcia <ricardo.delfin.garcia@gmail.com>
2018-01-22 13:06:37 -0600

    Mod'ied MeshLoader to return map of TriangleMesh [ci skip]

--- include/maguey/mesh_loader.hpp
+++ include/maguey/mesh_loader.hpp
@@ -12,6 +12,9 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <unordered_map>
+
+#include <maguey/triangle_mesh.hpp>
 
 namespace maguey {
 
@@ -27,54 +30,66 @@ public:
     }
 
     /**
-     * Loads in the contents of the file, and fills up the three vectors with
-     * the vertex data stored in the file. Returns whether the operation
-     * succeded or not.
+     * Loads in the contents of the file, and returns a list of triangle meshes
+     * generated from the file (each file might contain multiple objects). The
+     * names can either be generated from the files or by the MeshLoader
+     * implementation by its own policy.
      * 
-     * @param path     The path of the file to load. Relative to the working
-     *                 directory.
-     * @param vertices A reference to the vertice vector that will be edited to
-     *                 contain the vertices specified in the file.
-     * @param normals  A reference to the normal vector that will be edited to
-     *                 contain the normals specified in the file.
-     * @param faces    A reference to the face vector that will be edited to
-     *                 contain the indices of the vertices and normals that
-     *                 make up the faces specified in the file. Specifically,
-     *                 each index in each face corresponds to both the vertex
-     *                 and normal.
+     * @param path           The path of the file to load. Relative to the
+     *                       working directory.
+     * @param error          Is set to true if there was an error. Else set to
+     *                       false.
+     * @param camera         Camera object used to create the TriangleMesh
+     *                       objects.
+     * @param vertexShader   Default vertex shader if none is specified by the
+     *                       model.
+     * @param geometryShader Default geometry shader if none is specified by
+     *                       the model.
+     * @param fragmentShader Default fragment shader if none is specified by
+     *                       the model.
      */
-    virtual bool loadFile(const std::string& path,
-                          std::vector<glm::vec4>& vertices, std::vector<glm::vec4>& normals,
-                          std::vector<glm::uvec3>& faces) const {
+    virtual std::unordered_map<std::string, TriangleMesh> loadFile(const std::string& path, bool& error,
+                                                                   Camera& camera,
+                                                                   const Shader& vertexShader = Shader(MESH_SHADER_VERT, false),
+                                                                   const Shader& geometryShader = Shader(MESH_SHADER_GEOM, false),
+                                                                   const Shader& fragmentShader = Shader(MESH_SHADER_VERT, false)) const {
+        error = false;
 
         std::ifstream fileStream(path);
 
-        if(!fileStream)
-            return false;
-        
+        if(!fileStream) {
+            error = true;
+            return;
+        }
+
         std::string fileContents = std::string(std::istreambuf_iterator<char>(fileStream),
                                                std::istreambuf_iterator<char>());
-        return loadString(fileContents, vertices, normals, faces);
+        return loadString(fileContents, error, camera, vertexShader, geometryShader, fragmentShader);
     }
     
     /**
-     * Uses the contents string to fill up the three vectors with the vertex
-     * data stored in the file. Returns whether the operation succeded or not.
+     * Uses the contents string to create a set of triangle meshes generated
+     * from the contents (each content string might contain multiple objects).
+     * The names can either be generated from the files or by the MeshLoader
+     * implementation by its own policy.
      * 
-     * @param contents The contents of the file format being loaded in.
-     * @param vertices A reference to the vertice vector that will be edited to
-     *                 contain the vertices specified in the contents.
-     * @param normals  A reference to the normal vector that will be edited to
-     *                 contain the normals specified in the contents.
-     * @param faces    A reference to the face vector that will be edited to
-     *                 contain the indices of the vertices and normals that
-     *                 make up the faces specified in the contents.
-     *                 Specifically, each index in each face corresponds to
-     *                 both the vertex and normal.
+     * @param contents       The contents of the file format being loaded in.
+     * @param error          Is set to true if there was an error. Else set to
+     *                       false.
+     * @param camera         Camera object used to create the TriangleMesh
+     *                       objects.
+     * @param vertexShader   Default vertex shader if none is specified by the
+     *                       model.
+     * @param geometryShader Default geometry shader if none is specified by
+     *                       the model.
+     * @param fragmentShader Default fragment shader if none is specified by
+     *                       the model.
      */
-    virtual bool loadString(const std::string& contents,
-                            std::vector<glm::vec4>& vertices, std::vector<glm::vec4>& normals,
-                            std::vector<glm::uvec3>& faces) const = 0;
+    virtual std::unordered_map<std::string, TriangleMesh>  loadString(const std::string& contents, bool& error,
+                                                                      Camera& camera,
+                                                                      const Shader& vertexShader = Shader(MESH_SHADER_VERT, false),
+                                                                      const Shader& geometryShader = Shader(MESH_SHADER_GEOM, false),
+                                                                      const Shader& fragmentShader = Shader(MESH_SHADER_VERT, false)) const = 0;
 
     ~MeshLoader() {
 
